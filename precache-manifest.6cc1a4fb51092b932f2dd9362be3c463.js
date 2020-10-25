102d0f9655ed76d6ab055661ba4a3ea8f20f6fd4
antagonist12 <masteringangel@gmail.com>
2020-10-25 16:16:26 +0700

    Updates

---
+++ precache-manifest.6cc1a4fb51092b932f2dd9362be3c463.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83353c45037968b7aea045938df94e17",
+    "url": "/Calendar-Apps-v1-React/index.html"
+  },
+  {
+    "revision": "1e14d9f13eb25d4766a8",
+    "url": "/Calendar-Apps-v1-React/static/css/main.30ad0e63.chunk.css"
+  },
+  {
+    "revision": "8a3be5284a7858233336",
+    "url": "/Calendar-Apps-v1-React/static/js/2.b10b56ee.chunk.js"
+  },
+  {
+    "revision": "32fb9f73882cf467122b724d78712b06",
+    "url": "/Calendar-Apps-v1-React/static/js/2.b10b56ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e14d9f13eb25d4766a8",
+    "url": "/Calendar-Apps-v1-React/static/js/main.1fac7bc7.chunk.js"
+  },
+  {
+    "revision": "5ba0751b7c906203eaa71485abd87b9e",
+    "url": "/Calendar-Apps-v1-React/static/js/main.1fac7bc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7f1a550720379463751",
+    "url": "/Calendar-Apps-v1-React/static/js/runtime-main.181e1803.js"
+  }
+]);
